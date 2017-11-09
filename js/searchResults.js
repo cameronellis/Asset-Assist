42c4bae92fb2036e0b0892fea7e186fe65fb5d01
Cameron Ellis <cameronellis71@gmail.com>
2017-11-09 12:36:07 -0800

    prettifyied html layout. Added js code to determine which row a user selects

--- js/searchResults.js
+++ js/searchResults.js
@@ -7,11 +7,25 @@ $(document).ready(function(){
     // iterate through all of the assts that the users own - filter them through the search query
     for(let i = 0; i < users_assets.length; i++){
         let itemNameToLowerCase = users_assets[i].itemName.toLowerCase();
+
         if(itemNameToLowerCase.indexOf(searchQueryToLowerCase) !== -1){
-            $("#table_body_content").append("<tr><td>" + users_assets[i].itemName + "</td><td><a href='tableDetails.html'>" 
-                + "<img src='" + users_assets[i].itemPicture + "' width='100' height='100' " 
-                + "alt='Item image' class='img-thumbnail'/></a></td></tr>");
+            $("#table_body_content").append(
+                "<tr id=" + i + ">" + 
+                    "<td>" + users_assets[i].itemName + "</td>" + 
+                    "<td>" + 
+                        "<a onclick='setIndexToDisplay(" + i + ")'>" + 
+                            "<img src='" + users_assets[i].itemPicture + "' width='100' height='100' alt='Item image' class='img-thumbnail'/>" + 
+                        "</a>" + 
+                    "</td>" + 
+                "</tr>"
+            );
         }
 
     }
 });
+
+// When the user clicks on an image, this function determines what data to 
+// display on the next page
+function setIndexToDisplay(i){
+    localStorage.setItem("indexToDisplay", i);
+}
