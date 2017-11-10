c94ab843b627950ca7fd892e46ca71be6cfc1265
Cameron Ellis <cameronellis71@gmail.com>
2017-11-09 21:40:25 -0800

    Redesigned item details page and inserted an edit button

--- js/itemDetails.js
+++ js/itemDetails.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
 	let dataToDisplay = usersAssets[indexToDisplay];
 
 	$("#itemName").text(dataToDisplay.itemName);
-	$("#itemPrice").text("Price: $" + dataToDisplay.itemPrice);
-	$("#itemYear").text("Year of Purchase: " + dataToDisplay.itemYear);
-	$("#itemManufacturer").text("Manufacturer: " + dataToDisplay.itemManufacturer);
+	$("#itemPrice").text("$" + dataToDisplay.itemPrice);
+	$("#itemYear").text(dataToDisplay.itemYear);
+	$("#itemManufacturer").text(dataToDisplay.itemManufacturer);
 	$("#itemMiscNotes").text(dataToDisplay.itemMiscNotes);
 	$("#itemImage").attr("src", dataToDisplay.itemPicture);
 	$("#itemReceipt").attr("src", dataToDisplay.itemReceipt);
 });
 
 function deleteAsset(){
-	console.log("Item deleted button")
+	console.log("Item deleted button");
 }
