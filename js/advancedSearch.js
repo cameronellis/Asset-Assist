64efb19ada9a1ec55f8dcb9d77546307f7574617
Cameron Ellis <cameronellis71@gmail.com>
2017-11-10 10:57:08 -0800

    Code to set searchQuery for an advanced search

--- js/advancedSearch.js
+++ js/advancedSearch.js
@@ -10,16 +10,23 @@ document.getElementById("yearOfPurchase").innerHTML = options;
 
 // Navigate away warning on incomplete form when the user clicks the back button
 // in their browser or when the user tries to close the window
-window.onbeforeunload = function(){
-	return "leaving this page will reset the fields";
-}
+// window.onbeforeunload = function(){
+// 	return "leaving this page will reset the fields";
+// }
 
 $(document).ready(function() {
   // When the search button is clicked
   $("#searchButton").click(function() {
-    let itemName = $("#itemName").val();
+    let assetName = $("#assetName").val();
     let priceRange = $("#priceRange").val();
     let yearOfPurchase = $("#yearOfPurchase").val();
-    let manufacturer = $("#manufacturer").val();
+    let assetManufacturer = $("#assetManufacturer").val();
+    let searchQuery = {
+                        assetName: assetName, priceRange: priceRange, 
+                        yearOfPurchase: yearOfPurchase, assetManufacturer: assetManufacturer
+                      };
+
+    // update search query
+    localStorage.setItem("searchQuery", JSON.stringify(searchQuery));
   });
 });
