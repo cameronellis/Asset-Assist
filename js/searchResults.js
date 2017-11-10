f18366996ce4aa2855f41cab56c049c5f29d5524
Cameron Ellis <cameronellis71@gmail.com>
2017-11-10 10:55:34 -0800

    refactoring code to handle advanced searches

--- js/searchResults.js
+++ js/searchResults.js
@@ -5,22 +5,30 @@ $(document).ready(function(){
     let searchQueryToLowerCase = searchQuery.toLowerCase();
 
     // iterate through all of the assts that the users own - filter them through the search query
-    for(let i = 0; i < users_assets.length; i++){
-        let itemNameToLowerCase = users_assets[i].itemName.toLowerCase();
+    // if the search being performed is a basic search
+    if((typeof searchQuery) === "string"){
+        for(let i = 0; i < users_assets.length; i++){
+            let itemNameToLowerCase = users_assets[i].itemName.toLowerCase();
 
-        if(itemNameToLowerCase.indexOf(searchQueryToLowerCase) !== -1){
-            $("#table_body_content").append(
-                "<tr id=" + i + ">" + 
-                    "<td>" + users_assets[i].itemName + "</td>" + 
-                    "<td>" + 
-                        "<a onclick='setIndexToDisplay(" + i + ")'>" + 
-                            "<img src='" + users_assets[i].itemPicture + "' width='100' height='100' alt='Item image' class='img-thumbnail'/>" + 
-                        "</a>" + 
-                    "</td>" + 
-                "</tr>"
-            );
+            if(itemNameToLowerCase.indexOf(searchQueryToLowerCase) !== -1){
+                $("#table_body_content").append(
+                    "<tr>" + 
+                        "<td>" + users_assets[i].itemName + "</td>" + 
+                        "<td>" + 
+                            "<a onclick='setIndexToDisplay(" + i + ")'>" + 
+                                "<img src='" + users_assets[i].itemPicture + "' width='100' height='100' alt='Item image' class='img-thumbnail'/>" + 
+                            "</a>" + 
+                        "</td>" + 
+                    "</tr>"
+                );
+            }           
         }
+    }
+    // if the search being performed is an advanced search
+    else if((typeof searchQuery) === "object"){
+        for(let i = 0; i < users_assets.length; i++){
 
+        }
     }
 });
 
