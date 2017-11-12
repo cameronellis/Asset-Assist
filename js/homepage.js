faffd2ed9eccb1765333488f1c2dbc229a548901
dravida1 <dravida@ucsd.edu>
2017-11-11 17:34:17 -0800

    should show nuber of assets on homepage.  Please try on your browser

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
 	//show who is logged in
 	let currentlyLoggedInAs = localStorage.getItem("currentlyLoggedInAs");
 	let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
+	
+	let user_assets = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_assets"));
 	$("#usersFullName").text("Logged in as: " + userData.fName + " " + userData.lName);	
-
+	
+	let numOfAssets = user_assets.length;
+	
+	// displays number of assets saved to account
+	$("numOfAssetsSection").text("You have " + numOfAssets + " assets."
 	
 });
