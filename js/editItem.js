0dfc9a584a7b91c160cf6339fc968dcc9310f1b0
Cameron Ellis <cameronellis71@gmail.com>
2017-11-12 22:50:00 -0800

    Added empty mandatory input field error checking for editItem page

--- js/editItem.js
+++ js/editItem.js
@@ -32,18 +32,74 @@ $(document).ready(function(){
   $("#itemManufacturerField").attr("value", dataToDisplay.itemManufacturer);
   $("#itemMiscNotesField").text(dataToDisplay.itemMiscNotes);
 
-  // console.log(usersAssets);
-
+  // When the submit button is clicked
   $("#submitButton").click(function(){
-    let newAssetField = { 
-                          itemName: $("#itemNameField").val(), itemPrice: $("#itemPriceField").val(),
-                          itemYear: $("#yearOfPurchase").val(), itemPicture: dataToDisplay.itemPicture,
-                          itemReceipt: dataToDisplay.itemReceipt, itemManufacturer: $("#itemManufacturerField").val(),
-                          itemMiscNotes: $("#itemMiscNotesField").val()
-                        }; 
-    // update the asset with the new information
-    usersAssets[indexToDisplay] = newAssetField;
-    localStorage.setItem(currentlyLoggedInAs + "_assets", JSON.stringify(usersAssets));
-    window.location = "../html/itemDetails.html";
+    let itemNameField    = $("#itemNameField").val();
+    let itemPrice        = $("#itemPriceField").val();
+    let itemYear         = $("#yearOfPurchase").val();
+    let itemPicture      = $("#itemImage").attr("src");
+    let itemManufacturer = $("#itemManufacturerField").val();
+
+    // check if all mandatory forms are filled in...
+    if( itemNameField !== "" && itemPrice !== "" && itemYear !== "" 
+        && itemManufacturer !== ""){
+      console.log("All mandatory fields are filled in");
+      // let newAssetField = { 
+      //                       itemName: $("#itemNameField").val(), itemPrice: $("#itemPriceField").val(),
+      //                       itemYear: $("#yearOfPurchase").val(), itemPicture: dataToDisplay.itemPicture,
+      //                       itemReceipt: dataToDisplay.itemReceipt, itemManufacturer: $("#itemManufacturerField").val(),
+      //                       itemMiscNotes: $("#itemMiscNotesField").val()
+      //                     }; 
+      // // update the asset with the new information
+      // usersAssets[indexToDisplay] = newAssetField;
+      // localStorage.setItem(currentlyLoggedInAs + "_assets", JSON.stringify(usersAssets));
+      // go to item information page
+      // window.location = "../html/itemDetails.html";
+    }   
+    // All fields not filled in, highlight which ones are and which ones are not 
+    else{
+      // if item name field is not provided
+      if(itemNameField === ""){
+        // Give input box a red border
+        $("#assetNameSection").addClass("has-error");
+        // Error Message
+        $("#assetNameError").text("Please provide an asset name");
+      }
+      else{
+        // Give input box a green border
+        $("#assetNameSection").addClass("has-success");
+      }
+
+      // if item price field is not provided
+      if(itemPrice === ""){
+        $("#assetPriceSection").addClass("has-error");
+        // Error Message
+        $("#assetPriceError").text("Please provide the asset's price");
+      }
+      else{
+        $("#assetPriceSection").addClass("has-success");
+      }
+
+      // if item year field is not provided
+      if(itemYear === ""){
+        $("#assetYearSection").addClass("has-error");
+        // Error Message
+        $("#assetYearError").text("Please provide the asset's year of purchase");
+      }
+      else{
+        $("#assetYearSection").addClass("has-success");
+      }
+
+      // if item manufacturer is not provided 
+      if(itemManufacturer === ""){
+        $("#assetManufacturerSection").addClass("has-error");
+        // Error Message
+        $("#assetManufacturerError").text("Please provide the asset's manufacturer");
+      }
+      else{
+        $("#assetManufacturerSection").addClass("has-success");
+      }
+    }
+
   });
 });
