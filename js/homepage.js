8d6f6bc15cea3a6c81f7b7456eb516c612f5c202
Cameron Ellis <cameronellis71@gmail.com>
2017-11-13 17:31:44 -0800

    changed 'Welcome, Howard' to 'Welcome, [User]

--- js/homepage.js
+++ js/homepage.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
-	//show who is logged in
+	//show who is logged in in the Navigation menu
 	let currentlyLoggedInAs = localStorage.getItem("currentlyLoggedInAs");
 	let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
-	
 	let user_assets = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_assets"));
 	$("#usersFullName").text("Logged in as: " + userData.fName + " " + userData.lName);	
 	
-	let numOfAssets = user_assets.length;
+	$("#usersFirstName").text("Welcome, " + userData.fName + "!");
 
-	let assetsSum = 0;
-	
 	// displays number of assets saved to account
+	let numOfAssets = user_assets.length;
 	$("#numOfAssetsSection").text("Total assets saved: " + numOfAssets);
 	
+	// Calculate and display total value of all of the assets owned
+	let assetsSum = 0;
 	for(let i = 0; i < numOfAssets; i++) {
 		assetsSum += parseInt(user_assets[i].itemPrice);
-	}
-	
+	}	
 	$("#totalValue").text("Total value of all assets: $" + assetsSum);
 });
