a28f3b88b9ac496eaf188ba945d378a0ff67662e
Cameron Ellis <cameronellis71@gmail.com>
2017-11-17 12:27:58 -0800

    Revert "Update and rename searchForItem.js to searchForAsset.js"
    This reverts commit 4391a393ce8cc544548a31b30537e13474ca51ad.

--- js/searchForItem.js
+++ js/searchForItem.js
@@ -1,10 +1,10 @@
 // show who is logged in
-let currentlyLoggedInAs = localStorage.getAsset("currentlyLoggedInAs");
+let currentlyLoggedInAs = localStorage.getItem("currentlyLoggedInAs");
 // retrieve recent searches from local storage
-let recentSearches = JSON.parse(localStorage.getAsset(currentlyLoggedInAs + "_recentSearches"));
+let recentSearches = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_recentSearches"));
   
 $(document).ready(function(){
-  let userData = JSON.parse(localStorage.getAsset(currentlyLoggedInAs + "_userdata"));
+  let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
 
 
   // populate recent searches
@@ -43,7 +43,7 @@ function updateRecentSearches(searchName){
   }
 
   // put recent searches back in local storage
-  localStorage.setAsset(currentlyLoggedInAs + "_recentSearches", JSON.stringify(recentSearches));
+  localStorage.setItem(currentlyLoggedInAs + "_recentSearches", JSON.stringify(recentSearches));
 
 }
 
@@ -55,9 +55,9 @@ function executeBasicSearch(){
     // Update recent searches
     updateRecentSearches(searchName);
     // set search query term here
-    localStorage.setAsset("searchQuery", searchName);
+    localStorage.setItem("searchQuery", searchName);
     // set search type to basic
-    localStorage.setAsset("searchType", "basic");
+    localStorage.setItem("searchType", "basic");
     // go to search results page
     window.location = "searchResults.html";
   }
@@ -66,15 +66,15 @@ function executeBasicSearch(){
     // Give input box a red border
     $("#searchNameSection").addClass("has-error");
     // Error Message
-    $("#searchNameError").text("Please provide an asset name to search");
+    $("#searchNameError").text("Please provide an item name to search");
   }
 }
 
 function recentSearchValueIndex(searchIndex){
-  // set searchQuery to recent search list asset
-  localStorage.setAsset("searchQuery", recentSearches[searchIndex]);
+  // set searchQuery to recent search list item
+  localStorage.setItem("searchQuery", recentSearches[searchIndex]);
   // set search type to basic
-  localStorage.setAsset("searchType", "basic");
+  localStorage.setItem("searchType", "basic");
   // go to search results page
   window.location = "searchResults.html";  
 }
