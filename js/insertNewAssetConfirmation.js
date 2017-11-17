86f11cb36e2b0310a6ffe1ada7663eb40f9cbd98
Cameron Ellis <Cameron@CameronEllis-PC.(none)>
2017-11-17 12:10:23 -0800

    Revert "Update insertNewAssetConfirmation.js by replacing all "item" with "asset""
    This reverts commit 62cc83a22731158a7a51024a28357818572fa354.

--- js/insertNewAssetConfirmation.js
+++ js/insertNewAssetConfirmation.js
@@ -18,27 +18,27 @@ for (i = 0; i < acc.length; i++) {
 
 // localStorage - retrieve information from it and populate fields
 $(document).ready(function(){
-	let currentUser = localStorage.getAsset("currentlyLoggedInAs");
+	let currentUser = localStorage.getItem("currentlyLoggedInAs");
 	// retrieve list of assets for the user
-	let usersAssets = JSON.parse(localStorage.getAsset(currentUser + "_assets"));
+	let usersAssets = JSON.parse(localStorage.getItem(currentUser + "_assets"));
 	// the last element in the list is the latest element to have been inserted
 	let assetToDisplay = usersAssets[usersAssets.length - 1];
 
 	// show who is logged in
-	let currentlyLoggedInAs = localStorage.getAsset("currentlyLoggedInAs");
-	let userData = JSON.parse(localStorage.getAsset(currentlyLoggedInAs + "_userdata"));
+	let currentlyLoggedInAs = localStorage.getItem("currentlyLoggedInAs");
+	let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
 	$("#usersFullName").text("Logged in as: " + userData.fName + " " + userData.lName);
 
 	// Display content in its respective place in the page
-	$("#assetName").text(assetToDisplay.assetName);
-	$("#assetPrice").text("$ " + assetToDisplay.assetPrice);
-	$("#assetYear").text(assetToDisplay.assetYear);
-	$("#assetManufacturer").text(assetToDisplay.assetManufacturer);
-	// asset image
-	$("#assetPicture").attr("src", assetToDisplay.assetPicture);
-	// asset receipt
-	$("#assetReceipt").attr("src", assetToDisplay.assetReceipt);
+	$("#itemName").text(assetToDisplay.itemName);
+	$("#itemPrice").text("$ " + assetToDisplay.itemPrice);
+	$("#itemYear").text(assetToDisplay.itemYear);
+	$("#itemManufacturer").text(assetToDisplay.itemManufacturer);
+	// item image
+	$("#itemPicture").attr("src", assetToDisplay.itemPicture);
+	// item receipt
+	$("#itemReceipt").attr("src", assetToDisplay.itemReceipt);
 
-	$("#assetMiscDetails").text(assetToDisplay.assetMiscNotes);
+	$("#itemMiscDetails").text(assetToDisplay.itemMiscNotes);
 
 });
