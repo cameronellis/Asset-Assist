f10f6afe2a70363af3b356b48cac9a61d293182a
Cameron Ellis <Cameron@CameronEllis-PC.(none)>
2017-11-25 00:17:52 -0800

    removed total asset costs code

--- js/homepage.js
+++ js/homepage.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 	let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
 	let user_assets = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_assets"));
 	let numOfAssets = user_assets.length;
-	let assetsSum = 0;
 	
 	$("#usersFullName").text("Logged in as: " + userData.fName + " " + userData.lName);	
 	
@@ -13,12 +12,6 @@ $(document).ready(function(){
 	// displays number of assets saved to account
 	$("#numOfAssetsSection").text("Total assets saved: " + numOfAssets);
 	
-	// Calculate and display total value of all of the assets owned
-	for(let i = 0; i < numOfAssets; i++) {
-		assetsSum += parseInt(user_assets[i].itemPrice);
-	}	
-	$("#totalValue").text("Total value of all assets: $" + assetsSum);
-
 	$("#viewAllAssetsButton").click(function(){
 		localStorage.setItem("searchQuery","");
 		window.location = "../html/searchResults.html";
