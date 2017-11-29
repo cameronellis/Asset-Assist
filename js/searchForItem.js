82651f3d6ab7c7c3bc8c8a7e7321dccaa2b2989f
Cameron Ellis <Cameron@CameronEllis-PC.(none)>
2017-11-28 17:26:58 -0800

    Get rid of recent searches feature

--- js/searchForItem.js
+++ js/searchForItem.js
@@ -1,18 +1,8 @@
 // show who is logged in
 let currentlyLoggedInAs = localStorage.getItem("currentlyLoggedInAs");
-// retrieve recent searches from local storage
-let recentSearches = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_recentSearches"));
   
 $(document).ready(function(){
   let userData = JSON.parse(localStorage.getItem(currentlyLoggedInAs + "_userdata"));
-
-  // populate recent searches
-  for(let i = 0; i < recentSearches.length; i++){
-    $("#recentSearchesSection").append(
-      "<a onclick='recentSearchValueIndex(" + i + ")'>" +
-        "<h3>" + recentSearches[i] + "</h3>" + 
-      "</a>");
-  }
 
   $("#viewAllAssetsButton").click(function(){
     localStorage.setItem("showAssetDeletedAlert","false");
@@ -32,39 +22,12 @@ $(document).ready(function(){
   });
 });
 
-function updateRecentSearches(searchName){
-  let numberOfRecentSearchesToDisplay = 4;
-  let searchNameNotInRecentSearches = true;
-
-  for(let i = 0; i < recentSearches.length; i++){
-    if(searchName === recentSearches[i]){
-      searchNameNotInRecentSearches = false;
-    }
-  }
-
-  if(searchNameNotInRecentSearches){
-    // append to front of array
-    recentSearches.unshift(searchName);
-  }
-
-  if(recentSearches.length > numberOfRecentSearchesToDisplay){
-    // remove last elem
-    recentSearches.pop();
-  }
-
-  // put recent searches back in local storage
-  localStorage.setItem(currentlyLoggedInAs + "_recentSearches", JSON.stringify(recentSearches));
-
-}
-
 function executeBasicSearch(){
   let searchName = $("#searchName").val();
 
   // Check if search box is filled
   if(searchName !== ""){
     localStorage.setItem("showAssetDeletedAlert","false");
-    // Update recent searches
-    updateRecentSearches(searchName);
     // set search query term here
     localStorage.setItem("searchQuery", searchName);
     // set search type to basic
@@ -79,17 +42,6 @@ function executeBasicSearch(){
     // Error Message
     $("#searchNameError").text("Please provide an item name to search");
   }
-}
-
-// When a link under "Your Recent Searches" has been clicked
-function recentSearchValueIndex(searchIndex){
-  localStorage.setItem("showAssetDeletedAlert","false");
-  // set searchQuery to recent search list item
-  localStorage.setItem("searchQuery", recentSearches[searchIndex]);
-  // set search type to basic
-  localStorage.setItem("searchType", "basic");
-  // go to search results page
-  window.location = "searchResults.html";  
 }
 
 // Reference: https://stackoverflow.com/questions/979662/how-to-detect-pressing-enter-on-keyboard-using-jquery
